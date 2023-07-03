f6a81f87b53b0907e8391e27e9522fe774bf561a
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-07-03 02:00:46 -0300

    Social Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,11 @@ const Navbar = () => {
         </div>
 
         {/* Mobile menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }>
                 <li className='py-6 text-2xl'>Home</li>
                 <li className='py-6 text-2xl'>About</li>
                 <li className='py-6 text-2xl'>Experience</li>
@@ -42,11 +46,30 @@ const Navbar = () => {
         </ul>
 
         {/* Social icons */}
-        <div className='flex fixed flex-col top-[%35] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
-                <li className='w-[160px] h-[60px] flex justify-between items-center'>
-                    <a href='https://www.linkedin.com/in/alerandazzo/'>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                     href='https://www.linkedin.com/in/alerandazzo/'>
                         Linkedin <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                     href='https://github.com/Randalf95'>
+                        Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                     href='https://www.linkedin.com/in/alerandazzo/'>
+                        Email <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                     href='https://www.linkedin.com/in/alerandazzo/'>
+                        Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
             </ul>
