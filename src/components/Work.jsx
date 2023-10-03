89c4cab70358eaf2526736646c5f1cb1f7f80f00
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-10-03 18:16:58 -0300

    change in margin

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -88,7 +88,7 @@ const Work = () => {
           </div>
         </div>
       </div>
-      <div className="h-20"></div> {/* Agregar margen inferior para separar de Contact */}
+      <div className="h-200"></div> {/* Agregar margen inferior para separar de Contact */}
     </div>
   );
 };
