194840b4fd266108f9e5eaafd40a6cd183e39b15
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-10-03 18:18:44 -0300

    change in margin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 mt-20">
         <form method='POST' action='https://getform.io/f/eac737ee-7ba6-46e3-a5ca-3215e9586d57' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
