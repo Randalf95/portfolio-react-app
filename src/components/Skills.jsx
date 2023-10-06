e0d64e6923d1108cdeaeba659d75027ef7815a1b
Gaston Dvoskin <dvoskingaston@gmail.com>
2023-10-06 17:52:38 -0300

    Fixed whitespace

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ import SQL from '../assets/SQL.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-full bg-[#0a192f]  text-gray-300'>
+    <div name='skills' className='w-full xl:h-screen bg-[#0a192f] text-gray-300 pt-20'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
